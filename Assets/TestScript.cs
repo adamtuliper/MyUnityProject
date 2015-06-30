935647578c02be8a7051b07e73ad0cedca83df98
Adam Tuliper <adam.tuliper@gmail.com>
2015-06-30 10:08:53 -0700

    My Changes

---
+++ Assets/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Starting up" + gameObject.name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        ///sasas
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
